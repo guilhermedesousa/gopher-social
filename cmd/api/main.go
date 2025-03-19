53a75f4b0a28b73b40c88cdd35d27ec78f33bd81
Guilherme Santos <guilherme.udsantos@mercadolivre.com>
2025-03-19 18:23:19 -0300

    :tada: initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	"github.com/guilhermedesousa/social/internal/env"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	cfg := config{
+		addr: env.GetString("ADDR", ":8080"),
+	}
+
+	app := &application{
+		config: cfg,
+	}
+
+	mux := app.mount()
+
+	log.Fatal(app.run(mux))
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/guilhermedesousa/social
+
+go 1.24.0
+
+require (
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
