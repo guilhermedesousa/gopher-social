bfae5d39b7e33029aa64d3f3226172717707cd83
guilhermedesousa <guilhermedesousa.dev@gmail.com>
2025-06-02 16:29:42 -0300

    feat: integrate zap logger for improved logging in API

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/guilhermedesousa/social/internal/env"
 	"github.com/guilhermedesousa/social/internal/store"
 	"github.com/joho/godotenv"
+	"go.uber.org/zap"
 )
 
 const version = "0.0.2"
@@ -45,6 +46,11 @@ func main() {
 		env: env.GetString("ENV", "development"),
 	}
 
+	// Logger
+	logger := zap.Must(zap.NewProduction()).Sugar()
+	defer logger.Sync()
+
+	// Database
 	db, err := db.New(
 		cfg.db.addr,
 		cfg.db.maxOpenConns,
@@ -52,20 +58,21 @@ func main() {
 		cfg.db.maxIdleTime,
 	)
 	if err != nil {
-		log.Panic(err)
+		logger.Fatal(err)
 	}
 
 	defer db.Close()
-	log.Println("database connection pool established")
+	logger.Info("database connection pool established")
 
 	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
 		store:  store,
+		logger: logger,
 	}
 
 	mux := app.mount()
 
-	log.Fatal(app.run(mux))
+	logger.Fatal(app.run(mux))
 }
